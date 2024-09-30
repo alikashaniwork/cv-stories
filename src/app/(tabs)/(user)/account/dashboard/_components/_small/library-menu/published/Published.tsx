f07def8ed7af27dfacb2dbd5f80a3842eab0939f
alikashanidev <alikashani092@gmail.com>
2024-09-30 19:15:58 +0330

    Fix library menu in small dashboard page

--- src/app/(tabs)/(user)/account/dashboard/_components/_small/library-menu/published/Published.tsx
+++ src/app/(tabs)/(user)/account/dashboard/_components/_small/library-menu/published/Published.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useFetchListNumbers from "@/src/queries/story/library/useFetchListNumbers";
+import Link from "next/link";
 
 export default function Published() {
     const { data, isPending } = useFetchListNumbers();
@@ -7,31 +8,48 @@ export default function Published() {
     const published = data?.published;
     return (
         <div>
-            <p className="font-sfb text-xl tracking-wide px-1">My Stories</p>
+            <p className="font-sfb text-neutral-300 text-xl tracking-wide px-1">
+                Published Stories
+            </p>
             <ul className="grid grid-cols-4 gap-2 mt-2">
-                <li className="h-20 col-span-2 flex flex-col justify-center rounded-2xl bg-neutral-800">
-                    <p className="px-4 font-sfb text-lg tracking-wide text-green-500">
-                        Short
-                    </p>
-                    <p className="text-center font-sfb text-2xl tracking-wide">
-                        {published?.shorts}
-                    </p>
+                <li className="col-span-2">
+                    <Link
+                        href="library?status=published&type=short"
+                        className="h-24 flex-col items-start bg-neutral-800 shadow-sm shadow-neutral-900 backdrop-blur-xl rounded-2xl gap-1"
+                    >
+                        <p className="px-6 text-green-600 font-sfb text-lg tracking-wide">
+                            Short
+                        </p>
+                        <p className="text-center self-center font-sfb text-3xl tracking-wide text-white">
+                            {published?.shorts}
+                        </p>
+                    </Link>
                 </li>
-                <li className="h-20 col-span-2 flex flex-col justify-center rounded-2xl bg-neutral-800">
-                    <p className="px-4 font-sfb text-lg tracking-wide text-rose-500">
-                        Series
-                    </p>
-                    <p className="text-center font-sfb text-2xl tracking-wide">
-                        {published?.serieses}
-                    </p>
+                <li className="col-span-2">
+                    <Link
+                        href="library?status=published&type=series"
+                        className="h-24 flex-col items-start bg-neutral-800 shadow-sm shadow-neutral-900 backdrop-blur-xl rounded-2xl gap-1"
+                    >
+                        <p className="px-6 text-rose-600 -mt-2 font-sfb text-lg tracking-wide">
+                            Series
+                        </p>
+                        <p className="text-center self-center font-sfb text-3xl tracking-wide text-white">
+                            {published?.serieses}
+                        </p>
+                    </Link>
                 </li>
-                <li className="h-20 col-span-4 flex flex-col justify-center rounded-2xl bg-neutral-800">
-                    <p className="px-4 font-sfb text-lg tracking-wide text-blue">
-                        Novel
-                    </p>
-                    <p className="text-center font-sfb text-2xl tracking-wide">
-                        {published?.novels}
-                    </p>
+                <li className="col-span-4">
+                    <Link
+                        href="library?status=published&type=novel"
+                        className="h-24 flex-col items-start bg-neutral-800 shadow-sm shadow-neutral-900 backdrop-blur-xl rounded-2xl gap-1"
+                    >
+                        <p className="px-6 text-purple-500 -mt-2 font-sfb text-lg tracking-wide">
+                            Novel
+                        </p>
+                        <p className="text-center self-center font-sfb text-3xl tracking-wide text-white">
+                            {published?.novels}
+                        </p>
+                    </Link>
                 </li>
             </ul>
         </div>
